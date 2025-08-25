273119fe835992f65ea5c2ca7fc22ab6255d44cd
Edwin <codeninja.hero@gmail.com>
2025-08-25 00:01:00 -0500

    fix: change to https

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const javaAPI = axios.create({
-  baseURL: "http://partial-sabine-idiomago-c4716f6f.koyeb.app/idiomago",
+  baseURL: "https://partial-sabine-idiomago-c4716f6f.koyeb.app/idiomago",
 });
 
 const javaOauth =
-  "http://partial-sabine-idiomago-c4716f6f.koyeb.app/idiomago/oauth2/authorization";
+  "https://partial-sabine-idiomago-c4716f6f.koyeb.app/idiomago/oauth2/authorization";
 export { javaAPI, javaOauth };
